34c0f96f9113af1581b0776c1f71081cf2116793
Brian Li <brian@brianli.com>
2022-02-21 15:21:03 +0900

    Initial commit.

---
+++ omm_monk_cli/icx.py
@@ -0,0 +1,60 @@
+import os
+import typer
+from iconsdk.exception import JSONRPCException
+from iconsdk.icon_service import IconService
+from iconsdk.builder.call_builder import CallBuilder
+from iconsdk.providers.http_provider import HTTPProvider
+from iconsdk.wallet.wallet import KeyWallet
+from iconsdk.builder.transaction_builder import CallTransactionBuilder
+from iconsdk.signed_transaction import SignedTransaction
+from dotenv import load_dotenv
+from time import sleep
+
+load_dotenv()
+
+
+class Icx:
+
+    ICON_SERVICE = IconService(HTTPProvider("https://ctz.solidwallet.io", 3))
+
+    WALLET = KeyWallet.load(bytes.fromhex(os.getenv("WALLET_PK")))
+    WALLET_ADDRESS = WALLET.get_address()
+
+    MONK_MULTISIG_CONTRACT = "cx0c436b120f3eabeb538b14fd30505917c3f35ee0"
+
+    TICKER_TO_CONTRACT = {"OMM": "cx1a29259a59f463a67bb2ef84398b30ca56b5830a"}
+
+    CONTRACT_TO_EXA = {
+        "cx1a29259a59f463a67bb2ef84398b30ca56b5830a": 18,  # OMM
+    }
+
+    def __init__(self) -> None:
+        pass
+
+    def call(self, to: str, method: str, params: dict = {}):
+        call = CallBuilder().to(to).method(method).params(params).build()
+        try:
+            result = self.ICON_SERVICE.call(call)
+            return result
+        except JSONRPCException as e:
+            typer.echo(e)
+            raise typer.Exit()
+
+    def call_tx(self, to_address: str, method: str, params: dict = {}):
+        transaction = (
+            CallTransactionBuilder()
+            .from_(self.WALLET_ADDRESS)
+            .to(to_address)
+            .nid(1)
+            .value(0)
+            .method(method)
+            .params(params)
+            .build()
+        )
+        try:
+            signed_transaction = SignedTransaction(transaction, self.WALLET, 200000000)
+            tx_hash = self.ICON_SERVICE.send_transaction(signed_transaction)
+            return tx_hash
+        except JSONRPCException as e:
+            typer.echo(e)
+            raise typer.Exit()
---
+++ omm_monk_cli/main.py
@@ -0,0 +1,41 @@
+import json
+import typer
+from omm_monk_cli.icx import Icx
+
+app = typer.Typer()
+icx = Icx()
+
+
+@app.command()
+def hello():
+    typer.echo("Hello")
+
+
+@app.command()
+def submit_token_transfer(value: int, token_contract: str, to_address: str):
+    """
+    Calls the submitTransaction method.
+    """
+    if token_contract[:2] != "cx":
+        try:
+            token_contract = icx.TICKER_TO_CONTRACT[token_contract]
+        except KeyError as e:
+            typer.echo(f"ERROR: {token_contract} is not a supported token.")
+            raise typer.Exit()
+    exa = icx.CONTRACT_TO_EXA[token_contract]
+    params = {
+        "_destination": token_contract,
+        "_method": "transfer",
+        "_params": json.dumps({"_to": to_address, "_value": value * (10**exa)}),
+    }
+    tx_hash = icx.call_tx(icx.MONK_MULTISIG_CONTRACT, "submitTransaction", params)
+    typer.echo(tx_hash)
+
+
+@app.command()
+def confirm_transaction(transaction_id: int):
+    """
+    Calls the confirmTransaction method.
+    """
+    tx_hash = icx.call_tx(icx.MONK_MULTISIG_CONTRACT, "confirmTransaction", {"_transactionId": transaction_id})
+    typer.echo(tx_hash)
